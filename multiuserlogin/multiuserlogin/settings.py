13a78680522ab6f5b5b449dfdab5fe53b8c66a0d
REVANTHRAMESH S <110923763+REVANTH-2003@users.noreply.github.com>
2023-04-26 07:02:24 +0530

    settings update

--- multiuserlogin/multiuserlogin/settings.py
+++ multiuserlogin/multiuserlogin/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'multiuserlogin.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+'default': {
+
+'ENGINE': 'django.db.backends.mysql',
+
+'NAME': 'systematicfarming',
+
+'USER':'root', 
+
+'PASSWORD':'root', 
+} 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
